255ad8fda5a2ea1909efd58d92f301e685d17f25
tomaszfelczyk <tomaszfelczyk@buildroot.europe-west1-b.c.signal-os-devops-testing.internal>
2023-04-16 17:52:23 +0000

    install deps script

---
+++ install-deps.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+apt-get update && apt-get install -y build-essential bash bc binutils build-essential bzip2 cpio g++ gcc git gzip locales libncurses5-dev libdevmapper-dev libsystemd-dev make mercurial whois patch perl python rsync sed tar vim \ 
+    unzip wget bison flex libssl-dev libfdt-dev
