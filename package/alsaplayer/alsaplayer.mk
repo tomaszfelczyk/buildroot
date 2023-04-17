fdf57a667e0c7963c4ce67ae7709bde88f523ca6
tomaszfelczyk <tomasz.felczyk@gmail.com>
2023-04-17 17:11:32 +0000

    alsaplayer building

--- package/alsaplayer/alsaplayer.mk
+++ package/alsaplayer/alsaplayer.mk
@@ -5,27 +5,36 @@
 ################################################################################
 
 ALSAPLAYER_VERSION = 0.99.81
-ALSAPLAYER_SOURCE = alsaplayer-$(ALSAPLAYER_VERSION).tar.bz2
+ALSAPLAYER_SOURCE = alsaplayer-$(ALSAPLAYER_VERSION).tar.gz
 ALSAPLAYER_SITE = https://alsaplayer.sourceforge.net
-ALSAPLAYER_LICENSE = GPL-2.0
+ALSAPLAYER_LICENSE = GPL-3.0+
 ALSAPLAYER_LICENSE_FILES = COPYING
-ALSAPLAYER_DEPENDENCIES = alsa-lib libogg libmad libvorbis
-ALSAPLAYER_CFLAGS = $(TARGET_CFLAGS)
+ALSAPLAYER_CFLAGS = $(TARGET_CFLAGS) -DEMBEDDED
+ALSAPLAYER_AUTORECONF = YES
+ALSAPLAYER_DEPENDENCIES = host-pkgconf alsa-lib libmad libogg libvorbis
 
-ALSAPLAYER_CONF_OPTS =  --prefix=$(TARGET_DIR)/usr --disable-nls --disable-jack --disable-gtk2 --disable-esd --disable-opengl --disable-oggvorbis
+ALSAPLAYER_CONF_OPTS = \
+	--prefix=/usr --enable-mad \
+	--enable-shared=no \
+	--disable-option-checking \
+	--disable-audiofiletest \
+	--disable-vorbistest \
+	--disable-oggtest \
+	--disable-oss \
+	--disable-nas \
+	--disable-nls \
+	--disable-jack \
+	--disable-gtk2 \
+	--disable-esd \
+	--disable-opengl
 
-ALSAPLAYER_CONF_ENV = \
-	CFLAGS="$(ALSAPLAYER_CFLAGS)" \
-	LDFLAGS="$(TARGET_LDFLAGS) -lm"
-
-
-# Build and installation rules
+# # Build and installation rules
 define ALSAPLAYER_BUILD_CMDS
     $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
 endef
 
 define ALSAPLAYER_INSTALL_TARGET_CMDS
-    $(MAKE) DESTDIR=$(TARGET_DIR) $(TARGET_CONFIGURE_OPTS) -C $(@D) install
+    $(MAKE) DESTDIR=$(TARGET_DIR) -C $(@D) install
 endef
 
 # Hook package into Buildroot infrastructure
